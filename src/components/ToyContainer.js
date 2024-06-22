a552eafe3e475e1b840d4236f4339557e45abe65
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-22 18:20:07 -0400

    Fix broken image for rex in db.json file, render all toys on pageload

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,27 @@
-import React from "react";
-import ToyCard from "./ToyCard";
+import React, { useEffect, useState } from "react"
+import ToyCard from "./ToyCard"
 
 function ToyContainer() {
+  const [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data))
+  }, [])
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-  );
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard
+          key={toy.id}
+          name={toy.name}
+          image={toy.image}
+          likes={toy.likes}
+        />
+      ))}
+    </div>
+  )
 }
 
-export default ToyContainer;
+export default ToyContainer
